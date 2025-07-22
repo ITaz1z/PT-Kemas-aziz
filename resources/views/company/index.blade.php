ab0374901a3841e7e5e2a41f3925ce1d544b0683
Aziz <ajizmarwan12@gmail.com>
2025-07-22 16:03:46 +0700

    beranda css

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,21 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5">
-    <h1 class="mb-4">PT Kemas</h1>
 
-    <h5><strong>Deskripsi:</strong></h5>
+<section class="hero-section">
+    <h1>PT Kemas</h1>
     <p>{{ $company->description }}</p>
+</section>
 
-    <h5><strong>Visi:</strong></h5>
-    <p>{{ $company->vision }}</p>
-
-    <h5><strong>Misi:</strong></h5>
-    <p>{{ $company->mission }}</p>
-
-    <h5><strong>Kontak:</strong></h5>
-    <p>Alamat: {{ $company->address }}</p>
-    <p>Telepon: {{ $company->phone }}</p>
-    <p>Email: {{ $company->email }}</p>
-</div>
+<section class="feature-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4 feature-box">
+                <i class="bi bi-bullseye"></i>
+                <h4>Visi</h4>
+                <p>{{ $company->vision }}</p>
+            </div>
+            <div class="col-md-4 feature-box">
+                <i class="bi bi-lightbulb"></i>
+                <h4>Misi</h4>
+                <p>{{ $company->mission }}</p>
+            </div>
+            <div class="col-md-4 feature-box">
+                <i class="bi bi-telephone"></i>
+                <h4>Kontak</h4>
+                <p>Alamat: {{ $company->address }}</p>
+                <p>Telepon: {{ $company->phone }}</p>
+                <p>Email: {{ $company->email }}</p>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
